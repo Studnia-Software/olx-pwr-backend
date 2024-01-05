8fcbbeeadbbbe252b1e87db45f93e6e45816e1c0
Filip <filipkwiek888@gmail.com>
2024-01-05 21:18:43 +0100

    Init setup

---
+++ src/main/java/com/olxpwr/olxpwrbackend/OlxPwrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.olxpwr.olxpwrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxPwrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxPwrBackendApplication.class, args);
+	}
+
+}
